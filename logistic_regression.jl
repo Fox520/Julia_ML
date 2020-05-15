d9f575a83976c8359d3c233b63c769110e650c0c
Thomas <speedracer520@gmail.com>
2020-05-15 17:55:52 +0200

    Update logistic_regression.jl

--- logistic_regression.jl
+++ logistic_regression.jl
@@ -119,19 +119,21 @@ cleaned_romantic = map_to_int(romantic_vector)
 
 # 2. Extract outcome and features
 # 2.1 Create outcome vector
-outcome_vector = Array{Float64, 1}(undef, size(m)[1])
+y = Array{Int64, 1}(undef, size(m)[1])
 for i in 1:size(m)[1] # rows
     temp_total = 0
     for j in 31:size(m)[2] # From G1 to G3
         temp_total += m[i,j]
     end
     # Find average of grade and set value in outcome 1 if average is > 10 else 0
-    if (temp_total/3) > 10
-        global outcome_vector[i] = 1 #1.0 conversions to integer aren't working as expected
+    if (temp_total/3) >= 10
+        global y[i] = 1
     else
-        global outcome_vector[i] = 0 #0.0
+        global y[i] = 0
     end
 end
+
+println(y)
 
 # 2.2 Update the changes to the matrix to the numerical equivalents
 # TODO: Create vectors for the features then concatenate them. Below not functioning as expected
@@ -176,30 +178,101 @@ for i in 1:size(m)[1]
     end
 end
 
-# Remove G1, G2 and G3 from matrix by writing all except unwanted
-new_matrix = Matrix{Int64}(undef, size(m)[1], size(m)[2]-2)
+# Remove G1, G2 and G3 from matrix by writing all except unwanted. Note: deleteat! won't work with 2 dims
+x = Matrix{Float64}(undef, size(m)[1], size(m)[2]-3)
+
 for i in 1:size(m)[1]
-    for j in 1:size(m)[2]-2
+    for j in 1:size(m)[2]-3
         # Merge the outcome value as the last index
-        if j == size(m)[2]-2
-            new_matrix[i,j] = outcome_vector[i]
+        if j == size(m)[2]-3
+            x[i,j] = y[i]
             continue
         end
-        new_matrix[i,j] = m[i,j]
+        x[i,j] = m[i,j]
     end
 end
 
-# Standadize
-# To avoid InexactError: Int64(Int64, -0.3780405275933236) convert matrix to float type
-new_matrix = convert(Array{Float64, 2}, new_matrix)
-m_fit = fit(ZScoreTransform, new_matrix, dims=2)
-
-matrix_standardized = StatsBase.transform(m_fit, new_matrix)
+# Standardize x
+m_fit = fit(ZScoreTransform, x, dims=2)
+x = StatsBase.transform(m_fit, x)
 
 # Split into testing and training
 TEST_PERCENT = 0.8
 # Calculate number of rows to use as test sample
-testing_row_length = trunc(Int64, size(matrix_standardized)[1] * TEST_PERCENT)
-testing_matrix = matrix_standardized[1:testing_row_length]
-training_matrix = matrix_standardized[testing_row_length: size(matrix_standardized)[1]]
+testing_row_length = trunc(Int64, size(x)[1] * TEST_PERCENT)
+training_x = x[1:testing_row_length-1, :]
+testing_x = x[testing_row_length: size(x)[1], :]
+
+training_y = y[1:testing_row_length-1, :]
+testing_y = y[testing_row_length: size(y)[1], :]
+
+
+# Concatenate a vector of 1 to feature matrix (training_x) to represent bias
+training_x = hcat(training_x, ones(Int64, size(training_x)[1]))
+testing_x = hcat(testing_x, ones(Int64, size(testing_x)[1]))
+
+# hypothesis becomes h(X) = X × Θ (a matrix multiplication).
+# hθ(x) = g((θT x))
+function hypothesis(v_theta, X)
+    z = transpose(v_theta) * X
+    return 1/(1+exp(-z))
+end
+# t = zeros(size(training_x)[2])
+# hypothesis(t,training_x[1, :])
+
+function sum_and_square_theta(arr)
+    # Exclude first element of theta
+#     return sum(arr[2:size(arr)[1]]) ^ 2
+    a = sum(arr) - arr[1]
+    return a^2
+
+end
+
+function cost_function(X, Y, λ, theta)
+    m = size(X)[1]
+#     nfeatures = size(X)[1] - 1
+#     theta = zeros(size(X)[2])
+    cost = 0
+    gradient = 0
+    for i in 1:m
+        x = X[i,:]
+        y = Y[i]
+        # cross entropy
+        cost += (1/m)*(-y*log(hypothesis(theta, x)) - ((1-y) * log(1-hypothesis(theta, x))))
+        cost += (λ/2m) * sum_and_square_theta(theta)
+        gradient = ((1/m)*x) * (hypothesis(theta, x) - y) + (λ/m) * theta
+#         println("Hypothesis: ",hypothesis(theta, x), ", Actual: ",y)
+        # old
+#         first_part = y * log(hypothesis(theta,x)) - (1-y) * log(1-hypothesis(theta, x))
+#         second_part = (λ/2m) * sum_and_square_theta(theta)
+#         cost += (1/m)*(first_part + second_part)
+        # Update parameters for next iteration
+        
+#         grad = ((1/m)*x) * (hypothesis(theta, x) - y) + (λ/m) * theta
+#         theta = theta - α * grad
+    end
+    
+    return cost, gradient
+
+    #
+end
+
+
+function gradient_descent(X, Y, theta, α, num_iterations, λ) # α -> learning rate
+    tcost = 0
+    m = size(X)[1]
+    for i in 1:num_iterations
+        cost, grad = cost_function(X, Y, λ, theta)
+        theta = theta - (α*grad)
+        tcost += cost
+    end
+    return theta, (tcost/num_iterations)
+end
+
+predicate_theta, cost = gradient_descent(training_x, training_y, zeros(size(training_x)[2]), 0.2, 200, 0.2)
+println("Average cost:", cost)
+for i in 1: 10
+    pred = hypothesis(predicate_theta, testing_x[i, :])
+    println("Predicted: ",pred, ", Actual: ",testing_y[i])
+end
 
